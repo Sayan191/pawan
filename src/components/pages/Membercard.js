caeae4a4823b612c07bf3ec85e2a8cc377fc849d
Pawan Singh <77768153+PawanSingh890@users.noreply.github.com>
2022-02-06 14:53:29 +0530

    Create Membercard.js

---
+++ src/components/pages/Membercard.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import { Box } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+export default function SimplePaper() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        "& > :not(style)": {
+          m: 1,
+          width: 128,
+          height: 145,
+          textAlign: "center",
+          paddingTop: 65,
+          justifyContent: "center",
+          alignItems: "center",
+          verticalAlign: "middle",
+          borderRadius: "25px",
+          border: "9px solid #3f51b5",
+        },
+      }}
+    >
+      <Paper elevation={18}>
+        <Typography sx={{ fontSize: 22 }} color="text.secondary" gutterBottom>
+          <b> Planning Porker</b>
+        </Typography>
+      </Paper>
+    </Box>
+  );
+}

2d8b11136b2ac68f9d0117e73727c833b8d5c93e
Sayan191 <sayantalukdar30@gmail.com>
2022-02-09 12:06:01 +0530

    yyy

--- src/components/pages/StoryPoints.js
+++ src/components/pages/StoryPoints.js
@@ -6,6 +6,8 @@ import Button from "@material-ui/core/Button";
 
 export default function SimplePaper() {
 
+  let array = ["1","2","3","4","5","8","10","15","20"];
+
   return (
     <Box
       sx={{
@@ -25,83 +27,23 @@ export default function SimplePaper() {
         },
       }}
     >
-      <Paper elevation={18}>
-        <Typography sx={{ fontSize: 22 }} color="text.secondary" gutterBottom>
-          <Button
-            onClick={() => {
-              localStorage.setItem('days', JSON.stringify("0"))
-            }}
-          >
-            0
-          </Button>
-        </Typography>
-      </Paper>
-      <Paper elevation={18}>
-        <Typography sx={{ fontSize: 22 }} color="text.secondary" gutterBottom>
-          <Button
-            onClick={() => {
-              localStorage.setItem('days', JSON.stringify("1"))
-            }}
-          >
-            1
-          </Button>
-        </Typography>
-      </Paper>
-      <Paper elevation={18}>
-        <Typography sx={{ fontSize: 22 }} color="text.secondary" gutterBottom>
-          <Button
-            onClick={() => {
-              localStorage.setItem('days', JSON.stringify("2"))
-            }}
-          >
-            2
-          </Button>
-        </Typography>
-      </Paper>
-      <Paper elevation={18}>
-        <Typography sx={{ fontSize: 22 }} color="text.secondary" gutterBottom>
-          <Button
-            onClick={() => {
-              localStorage.setItem('days', JSON.stringify("3"))
-            }}
-          >
-            3
-          </Button>
-        </Typography>
-      </Paper>
-      <Paper elevation={18}>
-        <Typography sx={{ fontSize: 22 }} color="text.secondary" gutterBottom>
-          <Button
-            onClick={() => {
-              localStorage.setItem('days', JSON.stringify("4"))
-            }}
-          >
-            4
-          </Button>
-        </Typography>
-      </Paper>
-      <Paper elevation={18}>
-        <Typography sx={{ fontSize: 22 }} color="text.secondary" gutterBottom>
-          <Button
-            onClick={() => {
-              localStorage.setItem('days', JSON.stringify("5"))
-            }}
-          >
-            5
-          </Button>
-        </Typography>
-      </Paper>
-      <Paper elevation={18}>
-        <Typography sx={{ fontSize: 22 }} color="text.secondary" gutterBottom>
-          <Button
-            onClick={() => {
-              localStorage.setItem('days', JSON.stringify("8"))
-            }}
-          >
-            8
-          </Button>
-        </Typography>
-      </Paper>
+      {array.map((days,index)=>{
+        return(
+          <div>
+          <Paper elevation={18} key = {index}>
+          <Typography sx={{ fontSize: 22 }} color="text.secondary" gutterBottom>
+            <Button
+              onClick={() => {
+                localStorage.setItem('days', JSON.stringify(days))
+              }}
+            >
+              {days}
+            </Button>
+          </Typography>
+        </Paper>
+        </div>
+        )
+      })}
     </Box>
   );
 }
